a03e2af624f0048592ff4b855340a83163762e9e
Katy Bennett <katyrs22@gmail.com>
2025-05-26 15:32:11 +0100

    Error handling for non existing topics

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,14 +1,18 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Grid, GridItem, Text } from "@chakra-ui/react";
 import ArticleCard from "./ArticleCard";
 
 function ArticleList({ articles }) {
   return (
     <Grid gap={6}>
-      {articles.map((article, i) => (
-        <GridItem key={i}>
-          <ArticleCard article={article} />
-        </GridItem>
-      ))}
+      {articles.length === 0 ? (
+        <Text>No articles found</Text>
+      ) : (
+        articles.map((article, i) => (
+          <GridItem key={i}>
+            <ArticleCard article={article} />
+          </GridItem>
+        ))
+      )}
     </Grid>
   );
 }
